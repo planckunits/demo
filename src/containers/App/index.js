6d256d55fc06c2f19914eb694d457837ac9a3927
elzup <guild0105@gmail.com>
2017-11-02 14:53:47 +0900

    Fix Routing

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -6,19 +6,19 @@ import Switcher from '../System/Switcher'
 import Tabs from '../Tabs'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+const Main = () => (
+  <div>
+    <PageHeader title={'Reif福島 杭デモ'} />
+    <Switcher />
+    <Tabs />
+  </div>
+)
+
 const App = () => (
   <Router>
     <Switch>
-      <Route
-        path="/fukushima-reif-2017"
-        component={() => (
-          <div>
-            <PageHeader title={'Reif福島 杭デモ'} />
-            <Switcher />
-            <Tabs />
-          </div>
-        )}
-      />
+      <Route path="/" component={Main} />
+      <Route path="/fukushima-reif-2017" component={Main} />
     </Switch>
   </Router>
 )
